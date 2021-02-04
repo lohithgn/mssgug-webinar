1bf911c5322014edb29a153fa90ad06e85876639
LohithGN <lohith@xamariners.com>
2021-02-04 15:53:14 +0800

    refactor, add key vault
    refactor, add key vault

--- infrastructure/state/todoapi-infra-state.ps1
+++ infrastructure/state/todoapi-infra-state.ps1
@@ -1,18 +1,42 @@
-$rgName="rg-todoapi-infrastate-dev"
-$storageAccountName="sttodoapiinfrastate001"
+param([Parameter(Mandatory=$true)][string]$servicePrincipalObjId)
+
+$rgName="rg-todoapi-infra"
+$storageAccountName="sttodoapiinfra001"
 $location="southeast asia"
 $devContainer="dev"
+$keyVaultName="kv-todoapi-infra"
+$keyName="pulumi"
 
-Write-Host "Creating Resource Group"
+Write-Host "Creating Infra State"
+
+Write-Host "`n`t Creating Resource Group"
 $null = az group create --location westus --name $rgName
 
-Write-Host "Creating Storage Account"
+Write-Host "`n`t Creating Storage Account"
 $null = az storage account create --name $storageAccountName --resource-group $rgName --location $location --sku Standard_LRS
 
-Write-Host "Getting Storage Key"
+Write-Host "`n`t Getting Storage Key"
 $key = az storage account keys list -g $rgName -n $storageAccountName --query '[0].value' 
 
-Write-Host "Creating Storage Container"
+Write-Host "`n`t Creating Storage Container"
 $null = az storage container create --name $devContainer --account-key $key --account-name $storageAccountName
 
+Write-Host "`n`t Creating Key Vault"
+$null = az keyvault create --name $keyVaultName --resource-group $rgName --location $location --enabled-for-template-deployment true --sku "standard"
 
+Write-Host "`n`t Checking for Key"
+$key = az keyvault key show --vault-name $keyVaultName --name $keyName
+if ($null -ne $key) 
+{
+    Write-Host "`n`t Key exists"
+}
+else 
+{
+    Write-Host "`n`t Creating key"
+    $key = az keyvault key create --name $keyName --vault-name $keyVaultName --kty "RSA" --protection "software" --size 2048
+}
+
+Write-Host "`n`t Add access policy to KV"
+$null = az keyvault set-policy --name $keyVaultName --object-id $servicePrincipalObjId --key-permissions get list encrypt decrypt
+
+Write-Host "Done"
