b8d184f1de1bd224def29a46e84f1959c0f73339
LohithGN <lohith@xamariners.com>
2021-02-04 17:20:34 +0800

    add todo api infra
    add todo api infra

---
+++ infrastructure/ToDoApiInfra/ResourcePrefixes.cs
@@ -0,0 +1,7 @@
+public class ResourcePrefixes
+{
+    public const string ResourceGroup = "rg-";
+    public const string AppServicePlan = "asp-";
+    public const string Storage = "st";
+    public const string FunctionApp = "func-";
+}
